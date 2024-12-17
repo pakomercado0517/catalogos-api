2e55b4496aba9d8bdb6a2e23eca5c0bef3a74a42
pakomercado0517 <sissyyayle0517@hotmail.com>
2024-12-17 09:53:25 -0600

    change development issues before update to deploy

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const { concordDb, betterwareDb } = require("./src/DbData/catalogos.js");
 
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, async () => {
     await companiesFunction.createCompanies();
     await concordDb();
     await betterwareDb();
-    await scrapingFunction["andrea"]();
-    await scrapingFunction["cklass"]();
-    await scrapingFunction["priceShoes"]();
-    await scrapingFunction["vianney"]();
+    // await scrapingFunction["andrea"]();
+    // await scrapingFunction["cklass"]();
+    // await scrapingFunction["priceShoes"]();
+    // await scrapingFunction["vianney"]();
     console.log("catalogues ready to use");
     console.log(`listening at ${PORT}`);
   });
